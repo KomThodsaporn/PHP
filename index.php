4b76ae777f29487410c7b8c55d87705e86895497
KomThodsaporn <664230040@webmail.npru.ac.th>
2025-07-24 08:47:32 +0700

    1.0

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Basic</title>
+</head>
+
+<h1>Welcome to PHP Basic</h1>
+<p>This is a simple PHP application.
+</p>
+<hr>
+<h1 style="color:red">Basic PHP Syntax</h1>
+<pre>
+        &lt;?p
+            echo "Hello World";
+        ?&gt;
+    </pre>
+<h2>Result</h2>
+<div style="color: blue;">
+    <?php
+    echo "Hello World<br>";
+    print "<span style=color:green;>Thodsaporn Kukunthod</span>";
+    ?>
+</div>
+<hr>
+<h1 style="color:red">MY MENU</h1>
+<ul>
+    <li><a href="W01_01_introPHP.php">Intro PHP</a></li>
+    <li><a href="W02_01_oddEvenNumber.php">Odd-Even-Number</a></li>
+    <li><a href="W02_02_Grade.php">Calculate Grade</a></li>
+</ul>
+
+</body>
+
+</html>
